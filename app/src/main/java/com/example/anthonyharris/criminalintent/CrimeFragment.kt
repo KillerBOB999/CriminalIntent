31c8143f76a707e1ab62442e1ad664027473cfd6
Anthony Harris <33495441+KillerBOB999@users.noreply.github.com>
2020-04-08 10:32:18 -0500

    Updated to fulfil lab 07 requirements

--- app/src/main/java/com/example/anthonyharris/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/example/anthonyharris/criminalintent/CrimeFragment.kt
@@ -3,7 +3,6 @@ package com.example.anthonyharris.criminalintent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,8 +15,10 @@ import java.util.*
 
 private const val TAG = "CrimeFragment"
 private const val ARG_CRIME_ID = "crime_id"
+private const val DIALOG_DATE = "DialogDate"
+private const val REQUEST_DATE = 0
 
-class CrimeFragment:Fragment() {
+class CrimeFragment:Fragment(), DatePickerFragment.Callbacks {
     private lateinit var crime: Crime
     private lateinit var titleField:EditText
     private lateinit var dateButton:Button
@@ -42,10 +43,6 @@ class CrimeFragment:Fragment() {
         titleField=view.findViewById(R.id.crime_title) as EditText
         dateButton=view.findViewById(R.id.crime_date) as Button
         solvedCheckBox=view.findViewById(R.id.crime_solved) as CheckBox
-        dateButton.apply {
-            text=crime.date.toString()
-            isEnabled=false
-        }
         return view
     }
 
@@ -91,11 +88,22 @@ class CrimeFragment:Fragment() {
                 crime.isSolved = isChecked
             }
         }
+        dateButton.setOnClickListener{
+            DatePickerFragment.newInstance(crime.date).apply{
+                setTargetFragment(this@CrimeFragment, REQUEST_DATE)
+                show(this@CrimeFragment.parentFragmentManager, DIALOG_DATE)
+            }
+        }
     }
 
     override fun onStop(){
         super.onStop()
         crimeDetailViewModel.saveCrime(crime)
+    }
+
+    override fun onDateSelected(date: Date){
+        crime.date = date
+        updateUI()
     }
 
     private fun updateUI() {
